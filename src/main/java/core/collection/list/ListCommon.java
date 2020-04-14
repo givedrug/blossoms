65c7b68ac13a4725c683818c8d2fc55c1928d968
givedrug <givedrug@outlook.com>
2020-04-14 19:30:24 +0800

    first commit

---
+++ src/main/java/core/collection/list/ListCommon.java
@@ -0,0 +1,27 @@
+package core.collection.list;
+
+/**
+* @Date: 2020/4/14 19:29
+* @Description: List常用操作
+*/
+public class ListCommon {
+
+    /**
+    * @Date: 2020/4/14 19:29
+    * @Description: Array转List
+    */
+    public void arrayToList(){
+
+    }
+
+    /**
+    * @Date: 2020/4/14 19:29
+    * @Description: List转Array
+    */
+    public void listToArray(){
+
+    }
+
+
+
+}
