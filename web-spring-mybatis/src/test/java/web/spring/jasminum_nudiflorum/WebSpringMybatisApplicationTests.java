8906153bc2312a371d186523738902192e3931fc
wangjinjin <wangjinjin@didiglobal.com>
2020-10-21 14:43:19 +0800

    feat：增加web-spring-mybatis模块

---
+++ web-spring-mybatis/src/test/java/web/spring/jasminum_nudiflorum/WebSpringMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package web.spring.jasminum_nudiflorum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
