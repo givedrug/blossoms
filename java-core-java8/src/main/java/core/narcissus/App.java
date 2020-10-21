7de3b2e3bf54ed7f6348bac53a1c132374b89335
givedrug <givedrug@outlook.com>
2020-10-21 14:18:56 +0800

    feat：添加java-core-java8、web-spring-aop模块

---
+++ java-core-java8/src/main/java/core/narcissus/App.java
@@ -0,0 +1,13 @@
+package core.narcissus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web-spring-aop/src/main/java/web/spring/jasminum_nudiflorum/WebSpringAopApplication.java
@@ -0,0 +1,13 @@
+package web.spring.jasminum_nudiflorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringAopApplication.class, args);
+    }
+
+}
