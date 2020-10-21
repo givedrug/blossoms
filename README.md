af490873973b27fd0cecb499d8de019889c42c1e
givedrug <givedrug@outlook.com>
2020-10-21 14:43:19 +0800

    feat：增加web-spring-mybatis模块

--- README.md
+++ README.md
@@ -3,17 +3,13 @@
 
 &nbsp;
 
-**Narcissus 水仙**
-
-Java核心
+**Narcissus 水仙 - Java核心**
 
 模块：java-core-java8（Java基本语法，基于Java8）
 
 &nbsp;
 
-**Jasminum Nudiflorum 迎春花**
-
-Web Spring
+**Jasminum Nudiflorum 迎春花 - Web Spring**
 
 模块：web-spring-aop（spring aop 示例）
 
---
+++ web-spring-mybatis/src/main/java/web/spring/jasminum_nudiflorum/WebSpringMybatisApplication.java
@@ -0,0 +1,13 @@
+package web.spring.jasminum_nudiflorum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSpringMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSpringMybatisApplication.class, args);
+    }
+
+}
