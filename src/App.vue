c220a366770accc13320f69961586c379cc7acb7
wangjinjin <wangjinjin@didiglobal.com>
2023-06-05 19:17:42 +0800

    feat：读取json配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,55 @@
-<script setup lang="ts">
+<script lang="ts">
+import type {PropType} from 'vue'
+import {defineComponent} from 'vue'
+import url_json from './config/url.json'
+
+interface UrlBlock {
+  url_name: string,
+  url_link: string
+}
+
+interface UrlList {
+  [index: number]: UrlBlock
+}
+
+interface TypeBlock {
+  type_name: string
+  url_list: UrlList
+}
+
+interface UrlJson {
+  [index: number]: TypeBlock
+}
+
+export default defineComponent({
+  props: {
+    url_json: {
+      type: Object as PropType<UrlJson>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      url_json: ''
+    }
+  },
+  mounted() {
+    this.url_json = url_json
+  }
+})
+
 </script>
 
 <template>
+  <div v-for="type_block in url_json">
+    {{ type_block.type_name }}
+    <span v-for="url_block in type_block.url_list">
+      <a :href="url_block.url_link">{{ url_block.url_name }}</a>
+    </span>
+  </div>
 
-  Hello
+  <div v-for="type_block in url_json">
+    {{ type_block.type_name }}
+  </div>
 
 </template>
